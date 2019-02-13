d305ab07a3452e1daad83f272bdd4da882ec4cf9
t-phill <taylor-phillips>
2019-02-13 09:29:54 -0600

    remove junk

--- pg.py
+++ pg.py
@@ -20,36 +20,37 @@ conn.commit() # <--- makes sure the change is shown in the database
 conn.close()
 #cur.close()
 
-# def pg_load_table(file_path, table_name, dbname, host, port, user, pwd):
-#     '''
-#     This function upload csv to a target table
-#     '''
-#     try:
-#         conn = psycopg2.connect(dbname=welldata, host=host, port=port,\
-#          user=taylorphillips, password=pwd)
-#         print("Connecting to Database")
-#         cur = conn.cursor()
-#         f = open(file_path, "r")
-#         # Truncate the table first
-#         cur.execute("Truncate {} Cascade;".format(table_name))
-#         print("Truncated {}".format(table_name))
-#         # Load table from the file with header
-#         cur.copy_expert("copy {} from STDIN CSV HEADER QUOTE '\"'".format(table_name), f)
-#         cur.execute("commit;")
-#         print("Loaded data into {}".format(table_name))
-#         conn.close()
-#         print("DB connection closed.")
+def pg_load_table(file_path, table_name, dbname, host, port, user, pwd):
+    '''
+    This function upload csv to a target table
+    '''
+    try:
+        conn = psycopg2.connect(dbname=welldata, host=host, port=port,\
+         user=taylorphillips, password=pwd)
+        print("Connecting to Database")
+        cur = conn.cursor()
+        f = open(file_path, "r")
+        # Truncate the table first
+        cur.execute("Truncate {} Cascade;".format(table_name))
+        print("Truncated {}".format(table_name))
+        # Load table from the file with header
+        cur.copy_expert("copy {} from STDIN CSV HEADER QUOTE '\"'".format(table_name), f)
+        cur.execute("commit;")
+        print("Loaded data into {}".format(table_name))
+        conn.close()
+        print("DB connection closed.")
 
-#     except Exception as e:
-#         print("Error: {}".format(str(e)))
-#         sys.exit(1)
+    except Exception as e:
+        print("Error: {}".format(str(e)))
+        sys.exit(1)
 
-# # Execution Example
-# file_path = '/Users/taylorphillips/galvanize/capstone/c.csv'
-# table_name = 'usermanaged.restaurants'
-# dbname = 'welldata'
-# host = 'localhost'
-# port = '5432'
-# user = 'taylorphillips'
-# pwd = ''
-# pg_load_table(file_path, table_name, dbname, host, port, user, pwd)
+# Execution Example
+file_path = '/Users/taylorphillips/galvanize/capstone/combined.csv'
+table_name = 'test'
+dbname = 'welldata'
+host = 'localhost'
+port = ''
+user = 'taylorphillips'
+pwd = ''
+pg_load_table(file_path, table_name, dbname, host, port, user, pwd)
+
