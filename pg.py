4c39828461c9387f6a1c1e642b30e5c406151ce2
t-phill <taylor-phillips>
2019-02-13 09:25:58 -0600

    psychopg2

--- pg.py
+++ pg.py
@@ -14,32 +14,11 @@ today = '2019-02-01'
 try:
     cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
 except:
-    print("I can't drop our test database!")
-
-
-# This is not strictly necessary but demonstrates how you can convert a date
-# to another format
-ts = datetime.strptime(today, '%Y-%m-%d').strftime("%Y%m%d")
-
-# import psycopg2
-
-# try:
-#     conn = psycopg2.connect(database = "projetofinal", user = "postgres", password = "admin", host = "localhost", port = "5432")
-# except:
-#     print("I am unable to connect to the database") 
-
-# cur = conn.cursor()
-try:
-    cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
-except:
-    print("I can't drop our test database!")
-
-out = cur.fetchall()
-print(out)
+    print("Nope")
 
 conn.commit() # <--- makes sure the change is shown in the database
 conn.close()
-cur.close()
+#cur.close()
 
 # def pg_load_table(file_path, table_name, dbname, host, port, user, pwd):
 #     '''
