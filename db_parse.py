f128b3724ba6cbfdca69574954670a52090fe1f8
t-phill <taylor-phillips>
2019-02-14 14:10:55 -0600

    piping changes

--- db_parse.py
+++ db_parse.py
@@ -37,7 +37,7 @@ def sum_parse():
 
     combined = pd.concat(frames)
 
-    combined.columns = combined.columns.str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
+    combined.columns = combined.columns.str.replace(' ', '_').str.replace('(', '').str.replace(')', '').str.replace('#','num')
 
     combined.columns = [x.lower() for x in combined.columns]
 
@@ -46,20 +46,6 @@ def sum_parse():
     return combined
 
 sum_parse()
-parse_excel('/Users/taylorphillips/Downloads/*.xlsx')
-
-
-
-#combined.to_excel("c.xlsx", header=True)
-
-#combined.to_csv("test.csv", index=False)
-
-#summary_excel = glob.glob("/Users/taylorphillips/galvanize/capstone/*.xlsx")
- 
-
-
-# CREATE TABLE production ("API Number" VARCHAR(255), "Production Date" VARCHAR(255), "Oil Produced (bbl)" VARCHAR(255), "Water Produced (bbl)" VARCHAR (255), "Gas Produced (Mcf)" VARCHAR(255), "Days Well Produced" VARCHAR(255), "Gravity of Oil" VARCHAR(255), "Casing Pressure" VARCHAR(255), "Tubing Pressure" VARCHAR(255), BTU VARCHAR(255), "Method of Operation" VARCHAR(255), "Water Disposition" VARCHAR(255), "PWT Status" VARCHAR(255), "Well Type" VARCHAR(255), Status VARCHAR(255), "Pool Code" VARCHAR(255), "Reported Date" DATE);
-
-# COPY production("API Number","Production Date","Oil Produced (bbl)","Water Produced (bbl)","Gas Produced (Mcf)","Days Well Produced","Gravity of Oil","Casing Pressure","Tubing Pressure",BTU, "Method of Operation","Water Disposition","PWT Status","Well Type",Status,"Pool Code","Reported Date") FROM '/Users/taylorphillips/galvanize/capstone/combined.csv' DELIMITER ',' CSV HEADER;
+#parse_excel('/Users/taylorphillips/Downloads/*.xlsx')
 
 
