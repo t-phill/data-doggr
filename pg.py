ee379246b01b58c030ae321e52c2e1e6ea06132b
t-phill <taylor-phillips>
2019-02-14 19:44:46 -0600

    minor changes

--- pg.py
+++ pg.py
@@ -22,18 +22,32 @@ try:
 except:
     print("Nope didn't work")
 
+
 try:
     cur.execute('DROP TABLE IF EXISTS threepro;')
     cur.execute(
     '''
     CREATE TABLE threepro AS
     SELECT api_number, reported_date, days_well_produced, oil_produced_bbl, water_produced_bbl, gas_produced_mcf, well_type, status, pool_code
-    FROM production
+    FROM pro
     WHERE reported_date > date '2019-02-01' - interval '3 years';
     '''
 )
 except:
     print("Nope didn't work1")
+
+try:
+    cur.execute('DROP TABLE IF EXISTS tenpro;')
+    cur.execute(
+    '''
+    CREATE TABLE tenpro AS
+    SELECT api_number, reported_date, days_well_produced, oil_produced_bbl, water_produced_bbl, gas_produced_mcf, well_type, status, pool_code
+    FROM pro
+    WHERE reported_date > date '2019-02-01' - interval '10 years';
+    '''
+)
+except:
+    print("Nope tenpro didn't work")
 
 cur.execute('DROP TABLE IF EXISTS opsum;')
 cur.execute(
@@ -43,9 +57,23 @@ cur.execute(
     SUM(threepro.gas_produced_mcf) as gas
     FROM threepro
     JOIN summary ON threepro.api_number = summary.api_num
-    GROUP BY api_number, operator_name, well_type;
+    GROUP BY api_number, operator_name;
     '''
 )
+
+cur.execute('DROP TABLE IF EXISTS optensum;')
+cur.execute(
+    '''
+    CREATE TABLE optensum AS
+    SELECT tenpro.api_number, summary.operator_name, SUM(tenpro.days_well_produced) as days, SUM(tenpro.oil_produced_bbl) as oil, SUM(tenpro.water_produced_bbl) as water,
+    SUM(tenpro.gas_produced_mcf) as gas
+    FROM tenpro
+    JOIN summary ON tenpro.api_number = summary.api_num
+    GROUP BY api_number, operator_name;
+    '''
+)
+
+
 op_wells = psql.read_sql('''
 SELECT operator_name, COUNT(operator_name) as well_count FROM opsum
 GROUP BY operator_name
@@ -59,69 +87,25 @@ GROUP BY operator_name
 ORDER BY idle_count DESC
 ''', conn)
 
-# test_wells = psql.read_sql('''
-# SELECT operator_name, COUNT(operator_name) as idle FROM opsum
-# WHERE days = 0 
-# GROUP BY operator_name
-# ORDER BY operator_name
-#     (SELECT operator_name, COUNT(operator_name) as total FROM opsum)
-# ''', conn)
+df = op_wells.merge(idle_wells, on='operator_name', how='left').fillna(0)
+df['idle_rate'] = df['idle_count']/df['well_count']
 
 
+op_wells10 = psql.read_sql('''
+SELECT operator_name, COUNT(operator_name) as well_count FROM optensum
+GROUP BY operator_name
+ORDER BY well_count DESC
+''', conn)
 
+idle_wells10 = psql.read_sql('''
+SELECT operator_name, COUNT(operator_name) AS idle_count FROM optensum
+WHERE days = 0 
+GROUP BY operator_name
+ORDER BY idle_count DESC
+''', conn)
 
-# try:
-#     cur.execute('DROP TABLE IF EXISTS opsum;')
-#     cur.execute(
-#     '''
-#     CREATE TABLE opsum AS
-#     SELECT api_number, well_type, SUM(oil_produced_bbl) as oil, 
-#     SUM(water_produced_bbl) as water, 
-#     SUM(gas_produced_mcf) as gas,
-#     SUM(days_well_produced) as dval 
-#     FROM r
-#     GROUP BY api_number, well_type;
-# #     '''
-# )
-# except:
-#     print("Nope didn't work1")
-# try:
-#     cur.execute('DROP TABLE IF EXISTS op;')
-#     cur.execute(
-#     '''CREATE TABLE op AS
-#     SELECT ragg.api_number, summary.operator_name, ragg.well_type, ragg.oil, ragg.water, ragg.gas, ragg.dval
-#     FROM ragg
-#     JOIN summary
-#     ON ragg.api_number = summary.api_num;
-#     '''
-# )
-    # dataframe = psql.read_sql('''SELECT ragg.api_number, summary.operator_name, ragg.well_type, ragg.oil, ragg.water, ragg.gas, ragg.dval
-    #                         FROM ragg
-    #                         JOIN summary
-    #                         ON ragg.api_number = summary."api_#"''', conn)
-
-    
-
-#     #data2 = psql.read_sql('select operator_name, (count(*)/1856) as idle_rate from op where dval = 0 group by operator_name', conn)
-
-# except:
-#     print("Nope didn't work2")
-
-
-
-
-
-
-#'''select api_number, well_type, sum(oil_produced_bbl) as oil, sum(water_produced_bbl) as water, sum(gas_produced_mcf) as gas from recent group by api_number, well_type;'''
-# cur.execute(
-#     '''CREATE TABLE fulljoin AS
-#     SELECT *
-#     FROM test
-#     FULL OUTER JOIN summary
-#     ON test.api_number = summary."api_#";
-#     '''
-# )
-# #******pd.read_sql********
+df10 = op_wells10.merge(idle_wells10, on='operator_name', how='left').fillna(0)
+df10['idle_rate'] = df10['idle_count']/df10['well_count']
 
 
 
